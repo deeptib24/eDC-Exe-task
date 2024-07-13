dd647304909982ff2907633bac46a745b149dfb1
deeptib24 <deeptib1608@gmail.com>
2024-07-14 00:52:13 +0530

    edit in hero section+animations

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
     const themeSwitcher = document.getElementById('theme-switcher');
+    const seeMyWorksBtn = document.getElementById('see-my-works');
+    const projectsSection = document.getElementById('projects');
 
     themeSwitcher.addEventListener('click', () => {
         body.classList.toggle('dark');
@@ -16,9 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Initialize theme based on preference
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: )').matches) {
         body.classList.add('dark');
     } else {
         body.classList.add('light');
     }
+    seeMyWorksBtn.addEventListener('click', () => {
+        projectsSection.scrollIntoView({ behavior: "smooth" });
+    });
 });
